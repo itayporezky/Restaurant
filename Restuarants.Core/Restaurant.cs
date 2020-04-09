c92de6d1eedd6f59161facd49cab3dceece0d4b2
Itay <itay.porezky@gmail.com>
2020-04-09 15:04:45 +0300

    new setup

--- Restuarants.Core/Restaurant.cs
+++ Restuarants.Core/Restaurant.cs
@@ -6,9 +6,9 @@ namespace Restuarants.Core
 {
     public class Restaurant
     {
-        public int Id { get; set; };
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
-        
+        public CuisineType Cuisine { get; set; }
     }
 }
