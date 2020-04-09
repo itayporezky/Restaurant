53d4230f9e7949d5b804394af1cfd96307af98f9
Itay <itay.porezky@gmail.com>
2020-04-09 14:20:08 +0300

    moving git repository

---
+++ Restaurants.Data/IRestaurantsData.cs
@@ -0,0 +1,12 @@
+﻿using Restuarants.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurants.Data
+{
+    interface IRestaurantsData
+    {
+        IEnumerable<Restaurant>
+    }
+}
---
+++ Restuarants.Core/CuisineType.cs
@@ -0,0 +1,10 @@
+﻿namespace Restuarants.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Italian,
+        Indian
+    }
+}
