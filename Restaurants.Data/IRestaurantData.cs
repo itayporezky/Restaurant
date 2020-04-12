3c0782ac85128ec4eb8ae295317839c58bd04063
Itay <itay.porezky@gmail.com>
2020-04-12 18:59:49 +0300

    Adding search function to Restaurant/List

--- Restaurants.Data/IRestaurantData.cs
+++ Restaurants.Data/IRestaurantData.cs
@@ -8,7 +8,7 @@ namespace Restaurants.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
 
     }
 
@@ -21,13 +21,14 @@ namespace Restaurants.Data
             restaurants = new List<Restaurant>()
             {
                 new Restaurant { Id = 1, Name = "Number 1", Location = "Rishon", Cuisine = CuisineType.Indian },
-                new Restaurant { Id = 2, Name = "Number 2", Location = "Tel aviv", Cuisine = CuisineType.Mexican },
-                new Restaurant { Id = 3, Name = "Number 3", Location = "Ramla", Cuisine = CuisineType.Italian }
+                new Restaurant { Id = 2, Name = "Dud 5", Location = "Tel aviv", Cuisine = CuisineType.Mexican },
+                new Restaurant { Id = 3, Name = "Yoon 12", Location = "Ramla", Cuisine = CuisineType.Italian }
             };
         }
-        public IEnumerable<Restaurant> GetAll()
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
         }
