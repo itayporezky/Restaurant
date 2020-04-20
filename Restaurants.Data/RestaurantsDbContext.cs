82830227ac6e78fabc86aceb523dc45c6ff565d3
Itay <itay.porezky@gmail.com>
2020-04-20 23:24:26 +0300

    Started working on adding local db

---
+++ Restaurants.Data/RestaurantsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restuarants.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurants.Data
+{
+    public class RestaurantsDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
